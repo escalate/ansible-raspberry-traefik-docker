4c116b3aedfa00c807305ec9274c83f170f9b775
Felix Boerner <ich@felix-boerner.de>
2021-05-02 13:59:19 +0200

    feat: add molecule tests

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,60 @@
+"""Role testing files using testinfra"""
+
+
+def test_read_only_directories(host):
+    """Check read-only directories"""
+    f = host.file("/etc/traefik")
+    assert f.is_directory
+    assert f.user == "root"
+    assert f.group == "root"
+    assert f.mode == 0o755
+
+
+def test_traefik_config(host):
+    """Check Traefik config file"""
+    f = host.file("/etc/traefik/traefik.yml")
+    assert f.is_file
+    assert f.user == "root"
+    assert f.group == "root"
+
+    config = (
+                "accessLog: {}\n"
+                "api:\n"
+                "  insecure: true\n"
+                "entryPoints:\n"
+                "  web:\n"
+                "    address: :80\n"
+                "    http:\n"
+                "      redirections:\n"
+                "        entryPoint:\n"
+                "          scheme: https\n"
+                "          to: websecure\n"
+                "  websecure:\n"
+                "    address: :443\n"
+                "global:\n"
+                "  checkNewVersion: false\n"
+                "  sendAnonymousUsage: false\n"
+                "log:\n"
+                "  level: INFO\n"
+                "providers:\n"
+                "  docker:\n"
+                "    defaultRule: "
+                "Host(`{{ trimPrefix `/` .Name }}.docker.localhost`)\n"
+    )
+    assert config in f.content_string
+
+
+def test_traefik_service(host):
+    """Check Traefik service"""
+    s = host.service("traefik")
+    assert s.is_running
+    assert s.is_enabled
+
+
+def test_traefik_docker_container(host):
+    """Check Traefik docker container"""
+    d = host.docker("traefik.service").inspect()
+    assert d["HostConfig"]["Memory"] == 1073741824
+    assert d["Config"]["Image"] == "traefik:latest"
+    assert d["Config"]["Labels"]["maintainer"] == '"me@example.com"'
+    assert "TRAEFIK_API_DEBUG=True" in d["Config"]["Env"]
